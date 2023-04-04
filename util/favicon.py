458573e435127440833dc44e109a29ea4dcde866
Shinwoo Kim <sw2719@naver.com>
2023-04-04 10:49:55 +0900

    Remove debug code

--- util/favicon.py
+++ util/favicon.py
@@ -23,8 +23,3 @@ def get_favicon(url, headers=None):
 
     except requests.RequestException:
         return ''
-
-
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-
-print(get_favicon('https://www.danawa.com/', headers={'User-Agent': USER_AGENT}))
