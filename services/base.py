d85829c771c41e1ffc007b2890b177cf035b2d20
Shinwoo Kim <sw2719@naver.com>
2023-07-03 15:15:58 +0900

    Update user agent

--- services/base.py
+++ services/base.py
@@ -1,7 +1,8 @@
 import abc
+from fake_useragent import UserAgent
 from typing import Union, Dict
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'  # noqa
+USER_AGENT = UserAgent().chrome
 
 
 class AbstractService(abc.ABC):
